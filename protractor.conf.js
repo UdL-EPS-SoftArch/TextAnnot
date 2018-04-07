8d39fa76f80f340dcb18568972651a6a70ed92fa
Roberto García <roberto@rhizomik.net>
2018-04-07 19:43:01 +0200

    Enable chrome headless mode for e2e tests

--- protractor.conf.js
+++ protractor.conf.js
@@ -7,7 +7,10 @@ exports.config = {
     './e2e/**/*.feature'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=1024x768']
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
