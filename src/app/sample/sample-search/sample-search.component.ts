0c584aeb20401665eccff9e2ced56d1daf7a4eff
crisrivas <cris.rf95@gmail.com>
2018-05-06 19:03:56 +0200

    Perform sample-search.ts

--- src/app/sample/sample-search/sample-search.component.ts
+++ src/app/sample/sample-search/sample-search.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Sample } from "../sample";
+import { SampleService} from "../sample.service";
+
 
 @Component({
   selector: 'app-sample-search',
   templateUrl: './sample-search.component.html',
   styleUrls: ['./sample-search.component.css']
 })
-export class SampleSearchComponent implements OnInit {
+export class SampleSearchComponent{
+  @Input()
+  samples: Sample[];
+  @Output()
+  emitResults: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  public errorMessage: string;
+  constructor(private sampleService: SampleService) {
   }
 
+  performSearch(searchTerm: string): void {
+    this.sampleService.findByDescribedByName(searchTerm).subscribe(
+      samples => { this.emitResults.emit(samples); });
+  }
 }
