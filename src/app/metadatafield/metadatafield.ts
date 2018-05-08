693704ceda4c1a953a33aef293ff54b454b60848
oriol <oriolqueraltamoros@gmail.com>
2018-05-08 16:27:41 +0200

    added fields for metadataField

--- src/app/metadatafield/metadatafield.ts
+++ src/app/metadatafield/metadatafield.ts
@@ -4,7 +4,8 @@ import { Resource} from 'angular4-hal-aot';
 export class Metadatafield extends Resource {
   uri: string;
   id:string;
-  value: string;
-  forA: any;
-  valued: any;
+  name:string;
+  type:string;
+  values: string;
+  definedIn: string;
 }
