cce1bf96ea8a8a7b9c1283fd55a664790e28fd9c
Sergio Salcedo <trugove@hotmail.com>
2018-05-08 17:52:23 +0200

    Created list

---
+++ src/app/metadata-template/metadata-template-list/metadata-template-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metadata-template-list',
+  templateUrl: './metadata-template-list.component.html',
+  styleUrls: ['./metadata-template-list.component.css']
+})
+export class MetadataTemplateListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
