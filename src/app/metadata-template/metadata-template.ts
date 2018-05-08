75ef7462cb7944bd31da229166f9be2a4f9555e8
Sergio Salcedo <trugove@hotmail.com>
2018-05-08 18:03:52 +0200

    added metadata class

---
+++ src/app/metadata-template/metadata-template.ts
@@ -0,0 +1,9 @@
+import { Resource} from 'angular4-hal-aot';
+import {Sample} from "../sample/sample";
+
+
+export class MetadataTemplate extends Resource {
+  name: string;
+  defines: [{}];
+  describes: Sample[];
+}
