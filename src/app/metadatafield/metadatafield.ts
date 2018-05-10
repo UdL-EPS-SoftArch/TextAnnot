981657bfecdfec2e83e7620e522c58d97405f984
Rogercapcas <rogercapcas@gmail.com>
2018-05-10 16:21:20 +0200

    Minor changes

--- src/app/metadatafield/metadatafield.ts
+++ src/app/metadatafield/metadatafield.ts
@@ -2,9 +2,9 @@ import { Resource} from 'angular4-hal-aot';
 
 
 export class Metadatafield extends Resource {
-  id:string;
-  name:string;
-  type:string;
+  id: string;
+  name: string;
+  type: string;
   values: string;
   definedIn: string;
 }
