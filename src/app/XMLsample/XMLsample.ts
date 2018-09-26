f93c52f654188d8646aee11de432af6e81015ba5
Roberto García <roberto@rhizomik.net>
2018-09-26 17:58:58 +0200

    Implement XML upload form based on ng2-file-upload

--- src/app/XMLsample/XMLsample.ts
+++ src/app/XMLsample/XMLsample.ts
@@ -1,9 +1,12 @@
-
 import { Resource} from 'angular4-hal-aot';
-
 
 export class XMLSample extends Resource {
   uri: string;
   text: string;
   xml: string;
+
+  constructor(values: Object = {}) {
+    super();
+    Object.assign(this, values);
+  }
 }
