afe129fb10ad00d22b99316a6e82db0da1cdf5d1
Guillem96 <guillem.orellana@gmail.com>
2018-10-25 15:52:52 +0200

    Search working

--- src/app/tag-hierarchy/tag-hierarchy.component.ts
+++ src/app/tag-hierarchy/tag-hierarchy.component.ts
@@ -1,4 +1,6 @@
+import { TagHierarchyService } from './tag-hierarchy.service';
 import { Component, OnInit } from '@angular/core';
+import { TagHierarchy } from './tag-hierarchy';
 
 @Component({
   selector: 'app-tag-hierarchy',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tag-hierarchy.component.css']
 })
 export class TagHierarchyComponent implements OnInit {
+  public tagHierarchies: TagHierarchy[] = [];
 
-  constructor() { }
+  constructor(private tagHierarchyService: TagHierarchyService) { }
 
   ngOnInit() {
+    this.tagHierarchyService.getAll().subscribe(
+      res => this.tagHierarchies = res,
+      err => alert(err)
+    );
+  }
 
+  onSearch(tagHierarchies: TagHierarchy[]) {
+    this.tagHierarchies = tagHierarchies;
   }
 }
