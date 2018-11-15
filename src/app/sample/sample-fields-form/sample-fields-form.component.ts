2d5d03a73d7fb090d409d47af2a7ef2beb1773fe
adricasi <adricasi@users.noreply.github.com>
2018-11-15 16:45:08 +0100

    Display fields from MetadataTemplate.

--- src/app/sample/sample-fields-form/sample-fields-form.component.ts
+++ src/app/sample/sample-fields-form/sample-fields-form.component.ts
@@ -1,20 +1,20 @@
 import {Component, Input, OnChanges, OnInit, SimpleChange} from '@angular/core';
-import { Router } from '@angular/router';
-import { Sample } from '../sample';
-import { SampleService } from '../sample.service';
-import { MetadataTemplate } from '../../metadata-template/metadata-template';
-import { MetadataTemplateService } from '../../metadata-template/metadata-template.service';
+import {Metadatafield} from '../../metadatafield/metadatafield';
+import {MetadatafieldService} from '../../metadatafield/metadatafield.service';
 
 @Component({
   selector: 'app-sample-fields-form',
-  template: '<div class="form-group" style="width:100%;height:auto;padding: 20px; border:1px solid lightgray"> ' +
-  'These are my fields {{metadataTemplateUri}}</div>'
+  templateUrl: './sample-fields-form.html'
 })
-export class SampleFieldsFormComponent implements OnChanges {
-  @Input() metadataTemplateUri: String;
-
-  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
-    console.log("Something has changed: " + this.metadataTemplateUri);
+export class SampleFieldsFormComponent implements OnChanges, OnInit {
+  public metadataFields: Metadatafield[] = [];
+  constructor(private metadataService: MetadatafieldService) {}
+  @Input() metadataTemplateUri: string;
+  ngOnInit() {
+    this.metadataService.getMetadataFieldsByMetadataTemplate(this.metadataTemplateUri).subscribe(
+    (metadataFields: Metadatafield[]) => { this.metadataFields = metadataFields; });
   }
-
+  ngOnChanges(changes: {[propKey: string]: SimpleChange}) {
+    console.log('Something has changed: ' + this.metadataTemplateUri);
+  }
 }
