1e0c408b9ad86cd862f109c5e7e13a82c97c11dd
Guillem96 <guillem.orellana@gmail.com>
2018-12-13 15:49:09 +0100

    Show the metadata template of a metadata field

--- src/app/metadatafield/metadatafield-detail/metadatafield-detail.component.ts
+++ src/app/metadatafield/metadatafield-detail/metadatafield-detail.component.ts
@@ -1,9 +1,11 @@
+import { MetadataTemplateService } from './../../metadata-template/metadata-template.service';
 import { ModalService } from './../../shared/confirm-modal/modal.service';
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Metadatafield} from '../metadatafield';
 import { MetadatafieldService } from '../metadatafield.service';
 import { ConfirmModalComponent } from '../../shared/confirm-modal/confirm-modal.component';
+import { MetadataTemplate } from '../../metadata-template/metadata-template';
 
 @Component({
   selector: 'app-metadatafield-detail',
@@ -25,7 +27,12 @@ export class MetadatafieldDetailComponent implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     this.metadataFieldService.get(id).subscribe(
-      metadataField => this.metaField = metadataField);
+      metadataField => {
+        this.metaField = metadataField;
+        this.metaField.getRelation(MetadataTemplate, 'definedAt')
+          .subscribe(metadataTemplate => this.metaField.definedAt = metadataTemplate);
+      }
+    );
   }
 
   public delete() {
